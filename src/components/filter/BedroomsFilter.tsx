6904ea2ad22e6c1802bccdddb5495385f9fd763d
AchiGel <ar.gelashvili@gmail.com>
2024-09-15 15:06:01 +0400

    add bedrooms filter logic

--- src/components/filter/BedroomsFilter.tsx
+++ src/components/filter/BedroomsFilter.tsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
+import { PriceInput, PriceInputsContainer } from "./PriceFilter";
 import {
   ButtonArrow,
   FilterButton,
   FilterList,
+  FilterListTitle,
   FilterWrapper,
+  SelectButton,
 } from "./RegionFilter";
 
 export default function BedroomsFilter({
@@ -18,6 +22,19 @@ export default function BedroomsFilter({
   setAreaClicked: React.Dispatch<React.SetStateAction<boolean>>;
   setBedroomsClicked: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
+  const [selectedBedrooms, setSelectedBedrooms] = useState<number | "">("");
+
+  const handleBedroomsSelect = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value === "" ? "" : Number(e.target.value);
+
+    setSelectedBedrooms(value);
+  };
+
+  const handleBedrooms = () => {
+    console.log(selectedBedrooms);
+    setBedroomsClicked(false);
+  };
+
   return (
     <FilterWrapper>
       <FilterButton
@@ -31,7 +48,21 @@ export default function BedroomsFilter({
       >
         საძინებლების რაოდენობა <ButtonArrow direction={bedroomsClicked} />
       </FilterButton>
-      {bedroomsClicked ? <FilterList>Bedrooms filter</FilterList> : null}
+      {bedroomsClicked ? (
+        <FilterList>
+          <FilterListTitle>საძინებლების რაოდენობა</FilterListTitle>
+          <PriceInputsContainer>
+            <PriceInput
+              style={{ width: "20%" }}
+              type="number"
+              placeholder="2"
+              value={selectedBedrooms}
+              onChange={handleBedroomsSelect}
+            />
+          </PriceInputsContainer>
+          <SelectButton onClick={handleBedrooms}>არჩევა</SelectButton>
+        </FilterList>
+      ) : null}
     </FilterWrapper>
   );
 }
